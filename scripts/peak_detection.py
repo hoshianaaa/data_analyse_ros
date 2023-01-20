e29726e00604c57d0592168164d6c3b5faeb14bd
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-20 21:21:59 +0900

    update

--- scripts/peak_detection.py
+++ scripts/peak_detection.py
@@ -5,17 +5,31 @@
 import rospy
 from std_msgs.msg import *
 from geometry_msgs.msg import *
+from scipy import signal
+import numpy as np
 
 def callback(msg):
   print(msg.data)
+  y = np.array(msg.data)
+
+  maxid = signal.argrelmax(y, order=1) #最大値
+  minid = signal.argrelmin(y, order=1) #最小値
+
+  max_id_msg = Int32MultiArray()
+  min_id_msg = Int32MultiArray()
+
+  max_id_msg.data = maxid[0].tolist()
+  min_id_msg.data = minid[0].tolist()
+
+  max_pub.publish(max_id_msg)
+  min_pub.publish(min_id_msg)
 
 rospy.init_node("peak_detection")
-pub = rospy.Publisher("~peak_max_index", Int32MultiArray, queue_size=10)
-pub = rospy.Publisher("~peak_min_index", Int32MultiArray, queue_size=10)
-rospy.Subscriber("~data", Float32MultiArray, callback)
+max_pub = rospy.Publisher("~peak_max_index", Int32MultiArray, queue_size=10)
+min_pub = rospy.Publisher("~peak_min_index", Int32MultiArray, queue_size=10)
+rospy.Subscriber("/edge_detection/edge_graph", Float32MultiArray, callback)
 
 r = rospy.Rate(10)
 
 while not rospy.is_shutdown():
-  pub.publish(Float64(0))
   r.sleep()
