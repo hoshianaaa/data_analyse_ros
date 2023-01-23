6e901479ddd7215d7bd34eece0b90943e0c6025f
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-23 09:22:59 +0900

    update

--- scripts/peak_detection.py
+++ scripts/peak_detection.py
@@ -8,6 +8,8 @@ from geometry_msgs.msg import *
 from scipy import signal
 import numpy as np
 
+sensitivity_ = 50
+
 def callback(msg):
   print(msg.data)
   y = np.array(msg.data)
@@ -24,12 +26,21 @@ def callback(msg):
   max_pub.publish(max_id_msg)
   min_pub.publish(min_id_msg)
 
+def sensitivity_callback(msg):
+  print(msg)
+  global sensitivity_
+  sensitivity_ = msg.data
+
 rospy.init_node("peak_detection")
 max_pub = rospy.Publisher("~peak_max_index", Int32MultiArray, queue_size=10)
 min_pub = rospy.Publisher("~peak_min_index", Int32MultiArray, queue_size=10)
+sensitivity_state_pub = rospy.Publisher("/peak_detection/sensitivity_state", Float64, queue_size=10)
 rospy.Subscriber("/edge_detection/edge_graph", Float32MultiArray, callback)
+rospy.Subscriber("/edge_detection/edge_sensitivity", Float64, sensitivity_callback)
 
 r = rospy.Rate(10)
 
 while not rospy.is_shutdown():
+  sensitivity_state_pub.publish(Float64(sensitivity_))
+  print(sensitivity_)
   r.sleep()
